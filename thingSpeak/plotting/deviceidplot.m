308cd893bc686530f922fe0b62276e383d3181e6
Jerome-h <Jerome-h@users.noreply.github.com>
2019-06-01 16:33:18 +0100

    Update deviceidplot.m

--- thingSpeak/plotting/deviceidplot.m
+++ thingSpeak/plotting/deviceidplot.m
@@ -1,23 +1,21 @@
-% Template MATLAB code for visualizing data from a channel as a 2D line
-% plot using PLOT function.
+% Author: Jerome Hallett
+% Adapted from template MATLAB code for visualizing data from a channel as a 2D line
 
 % Prior to running this MATLAB code template, assign the channel ID to read
 % data from to the 'readChannelID' variable. Also, assign the field ID
 % within the channel that you want to read data from to plot. 
 
-% TODO - Replace the [] with channel ID to read data from:
+% Channel ID to read data from:
 readChannelID = [786238];
-% TODO - Replace the [] with the Field ID to read data from:
+% Field ID to read data from:
 fieldID1 = [1];
 fieldID2 = [2];
 
 % Channel Read API Key 
-% If your channel is private, then enter the read API
-% Key between the '' below: 
 readAPIKey = 'P5M089D9VZBO8CI2';
 
 %% Read Data %%
-samples = 30;
+samples = 10;
 
 % Read time variable
 [timedata] = thingSpeakRead(readChannelID,'OutputFormat','table', 'Field', fieldID1, 'NumPoints', samples, 'ReadKey', readAPIKey);
